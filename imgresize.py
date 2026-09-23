a270c94c8737a030a8b691f836c47644e3f40362
sammypatel06 <sammypatel06@gmail.com>
2026-09-23 11:58:24 -0500

    cleaned up grayscale function, set placeholders for some more functions

--- imgresize.py
+++ imgresize.py
@@ -5,25 +5,36 @@ import sys
 
 def loadGrayscale(path) -> np.ndarray:
     img = cv2.imread(path, 0) # cv2.IMREAD_GRAYSCALE
-
-    if not img.any(): # Check for missing file
-        raise FileNotFoundError(f"Image at {path} was not read")
-     
+    if not img: raise FileNotFoundError(f"Image not found at {path}") # Missng file 
     return img
 
 def show(*images, titles=None):
     fig, axes = plt.subplots(1, len(images), squeeze=False)
     for ax, img in zip(axes[0], images):
         ax.imshow(img, cmap="gray", vmin=0, vmax=255)
-        ax.set_axis_off()
+        ax.axis("off") # removes ticks on axes
+    if titles:
+        for ax, t in zip(axes[0], titles):
+            ax.set_title(t)
+    plt.tight_layout()
     plt.show()
+
+def isDarkOnLight(gray) -> bool:
+    # Take border strip (outer 5% of rows and cols)
+    # Compute median 
+    # Compare to 127
+    pass
+
+def binarize(img):
+    blur = cv2.GaussianBlur(img, (5,5), 0)
+    ret, th = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 if __name__ == "__main__":
     p = sys.argv[1]
     arr = loadGrayscale(p)
     print(f"shape: {arr.shape}")
     print(f"dtype: {arr.dtype}")
-    print(f"min  : {np.min(arr)}")
-    print(f"max  : {np.max(arr)}")
-    print(f"mean : {np.mean(arr)}")
+    print(f"min  : {arr.min()}")
+    print(f"max  : {arr.max()}")
+    print(f"mean : {arr.mean()}")
     show(arr)
